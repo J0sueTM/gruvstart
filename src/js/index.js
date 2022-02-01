c280fb3e381921c697f367a7a1100937d1966c08
J0sueTM <teodoro.josue@protonmail.ch>
2022-02-01 18:23:14 -0300

    Made sure links and normal queries are parsed differently

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,27 @@
 
 const ENTER_KEY = 13;
 
+const duckduckgoQuery = "https://duckduckgo.com/?q=";
+
+const urlPattern = new RegExp(
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'                      + // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'                  + // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'                         + // query string
+  '(\\#[-a-z\\d_]*)?$', 'i'                            // fragment locator
+);
+
+function isURL(url) {
+  return !!urlPattern.test(url);
+}
+
 const search = document.getElementById('search');
-search.on('keyup', (event) => {
-  if (event.keyCode !== ENTER_KEY)
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode !== ENTER_KEY && document.activeElement.tagName !== 'input')
     return;
 
-  var duckduckgoSecrh = "https://duckduckgo.com/?q=";
-  window.location.replace(newLink + search.value);
+  if (isURL(search.value))
+    window.location.replace('https://' + search.value);
+  else
+    window.location.replace(duckduckgoQuery + search.value);
 });
